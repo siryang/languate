a791fa9529ba71362d1110480d71be5679021028
siryang <sir_yangd@hotmail.com>
2014-05-21 15:47:01 +0800

    add demo of ruby_net

--- ruby/net/ruby_server.rb
+++ ruby/net/ruby_server.rb
@@ -1,3 +1,5 @@
+=begin
+#this another sample of http server
 require "socket"
  
 webserver = TCPServer.new('localhost', 880)
@@ -7,3 +9,39 @@ while (session = webserver.accept)
   session.close
 end
 puts "h"
+
+=end
+
+#the sample of a file web-server
+require 'webrick'
+
+trap 'INT' do server.shutdown end
+
+#web servlet, access by ip:port/simple
+class Simple < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET request, response
+    #status, content_type, body = do_stuff_with request
+    response.body = request.to_s + "\n\n"
+
+    response.status = 200
+    response['Content-Type'] = 'text/plain'
+    response.body += 'Hello, World! This is Servlet Simple!'
+  end
+end
+
+#create server
+root = File.expand_path Dir::pwd
+server = WEBrick::HTTPServer.new(:Port => 9090,
+	:DocumentRoot => Dir::pwd + "/")
+
+#mount access point
+server.mount_proc '/' do |req, res|
+	res.body = 'Hello, world!' + "req info:" + req.to_s
+end
+
+server.mount '/simple', Simple
+
+
+server.start
+
+
